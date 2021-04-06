450b2d6755fecdcde4e726689806929e324d2156
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2021-04-06 23:41:15 +0900

    notice

---
+++ notice.js
@@ -0,0 +1,9 @@
+function add_sub(){
+
+    var div = document.createElement('div');
+    div.innerHTML = document.getElementById('lecture_note').innerHTML;
+
+    document.getElementById('field').appendChild(div);
+
+}
+
