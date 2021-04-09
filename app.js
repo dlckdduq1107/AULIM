499dc1d2bbcf5eb007fbd65576256f4c691c0aa3
SeungheonShin <gody8756@ajou.ac.kr>
2021-04-09 20:26:26 +0900

    basic routing

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const express = require('express');
 const app = express();
 const path = require ('path');
 
-app.use(express.static(path.join(__dirname + '../public')));
+app.use(express.static(path.join(__dirname + '/public')));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('time_table.html');
+});
+
+app.get('/activity_stream', (req, res) => {
+    res.render('activity_stream.html');
+});
+
+app.get('/schedule', (req, res) => {
+    res.render('schedule.html');
+});
+
+app.get('/Group', (req, res) => {
+    res.render('Group.html');
 });
 
 app.listen(3000, () => {
