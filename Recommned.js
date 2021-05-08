65f38c69d43c86c04cf97fae655ecd2f766cb4db
kimjinsu <wlstnsp1@ajou.ac.kr>
2021-05-08 20:31:21 +0900

    aa

--- Recommned.js
+++ Recommned.js
@@ -1,3 +1,15 @@
+const { json } = require("express");
+
+class act{
+    constructor(name, classdate, start, long, alarm){
+        this.name = name;
+        this.classdate = classdate;
+        this.start = start;
+        this.long = long;
+        this.alarm = alarm;
+    }
+}
+
 function Recommend(inputarr){ // 추천시간표에 추가해야할 활동내용 
     $.getJSON('time_table' , function(data){ // 기존 시간표 읽어오기
         var rows = document.getElementById("time_table").getElementsByTagName("tr");
@@ -48,7 +60,12 @@ function Recommend(inputarr){ // 추천시간표에 추가해야할 활동내용
                             rowcell = 16;
                             break;
                     }
-                    a[i] = [rowcell, colcell];
+                    //a[i] = [rowcell, colcell];
+                    a[i] = new act();
+                    a[i].name = value.name;
+                    a[i].classdate = value.classdate;
+                    a[i].start = value.start;
+                    a[i].alarm = value.alarm;
                     for(var k = 0; k < value.long; k++){
                         var cells = rows[rowcell+k].getElementsByTagName("td");
                         cells[colcell].innerHTML = value.name;
@@ -66,15 +83,29 @@ function Recommend(inputarr){ // 추천시간표에 추가해야할 활동내용
             var y = Math.floor(math.random()*30)+1   //랜덤으로 시간 설정  
             var select = [x,y];    // a arr모양으로 랜덤값 설정
             for(var j = 0; j < a.length; j++){
-                if (select == a[j]){    // a arr에 랜덤값과 같은 값있으면 flag false로 바꿈 -> 다시 랜덤값 구해야함
-                    flag = false;
-                    break;
+                for(var k=0; k<a[j].start.length; k++){
+                    if(isNaN(a[j].start[k])){ //알파벳인 경우
+                        if (select[0] == a[j].classdate[k] && select[1] == a[j].start[k]
+                            && select[1] == a[j].start[k]+1 && select[1] == a[j].start[k]+2){    // a arr에 랜덤값과 같은 값있으면 flag false로 바꿈 -> 다시 랜덤값 구해야함
+                            flag = false;
+                            break;        
+                        } 
+                    }else{
+                        if (select[0] == a[j].classdate[k] && select[1] == a[j].start[k]
+                            && select[1] == a[j].start[k]+1){    // a arr에 랜덤값과 같은 값있으면 flag false로 바꿈 -> 다시 랜덤값 구해야함
+                            flag = false;
+                            break;        
+                        }
+                    }
                 }
+                if(flag == flase)
+                break;
             }
             if (flag){ // 랜덤값이 유효한 값인 경우
                // selectarr[i] = [x,y,inputarr[i].name, inputarr[i].long]; // selectarr에 추가함
-                selectarr[i] = new Object()
+                selectarr[i] = new act();
                 selectarr[i].name = inputarr[i].name;
+                selectarr[i].classdate = inputarr[i].classdate;
                 selectarr[i].start = inputarr[i].start;
                 selectarr[i].long = selectarr[i].long;
                 selectarr[i].alarm = selectarr[i].alarm;
@@ -95,7 +126,7 @@ function Recommend(inputarr){ // 추천시간표에 추가해야할 활동내용
             }
         }
             var jsonTT = JSON.stringify(selectarr);
-
+            
     })
 }
 
