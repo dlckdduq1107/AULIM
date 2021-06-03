1d5f3bf0cd953b2b1cec9f993375d29f872c400d
fansor <dlckdduq97@gmail.com>
2021-06-03 23:03:48 +0900

    print_empty_time

--- modules/empty_class.js
+++ modules/empty_class.js
@@ -1,83 +1,57 @@
-var table = [[],[],[],[],[]];
-var monTable  = [];
-var monCount = 0;
-
-function dayToInt(day) {
-    switch(day) {
-        case '월':
-            return 0;
-            break;
-        case '화':
-            return 1;
-            break;
-        case '수':
-            return 2;
-            break;
-        case '목':
-            return 3;
-            break;
-        case '금':
-            return 4;
-            break;
-        default:
-            return;
-    }
-}
 
 const empty = async(index) => {
-
    for (var i = 1; i <= index; i++) {//인원수만큼 반복
       $.getJSON(`../data/sample_empty/student${i}.json`, function(data){//제이슨 파일 읽기
-        // console.log(data);
+        var rows = document.getElementById("time_table").getElementsByTagName("tr");
+        var cells = rows[4].getElementsByTagName("td");
          for (var j = 0; j < data["activities"].length; j++) {//과목수만큼 반복
             for (var k = 0; k < data["activities"][j]["classdate"].length; k++) {//시간수만큼 반복
-                monTable.push(data["activities"][j]["start"][k]);//딕셔너리에 추가
-                //if(dayToInt(data["activities"][j]["classdate"][k]) == 0) monCount++;
+              switch(data["activities"][j]["classdate"][k]){
+                  case '월':
+                      colcell = 1;
+                      break;
+                  case '화':
+                      colcell = 2;
+                      break;
+                  case '수':
+                      colcell = 3;
+                      break;
+                  case '목':
+                      colcell = 4;
+                      break;
+                  case '금':
+                      colcell = 5;
+                      break;
+                  default:
+                      alert("error!");
+              }
+              var diff= data["activities"][j]["start"][k].charCodeAt(0) //ASCII Code로 변환함
+              var long=0;
+              if(Number(diff)>=65 && Number(diff)<=70){ //A~F의 값일 경우 조건문
+                  var pluscell= Number(diff)-Number('A'.charCodeAt(0))
+                  rowcell=1+3*pluscell
+                  long=3
+              }
+              else { //문자가 아닌 숫자형식
+                  var time_num = Number(data["activities"][j]["start"][k])
+                  var pluscell = time_num - 1 //2.5-1 =1.5
+                  rowcell = 1 + pluscell * 2
+                  long=2
+              }
+
+              //value.long[j]=long
+              for(var q=0;q<long;q++){
+                  var cells = rows[rowcell+q].getElementsByTagName("td");
+                  cells[colcell].innerHTML = "수업시간";
+              }
+
+
+
             }
          }
       });
    
     }
-    console.log(monTable);
-    find();
-}
-
-function find(){
-    var rowcell = 1;
-    var colcell = 0;
-    
-    console.log(monTable.length);
-    for(var idx=0; idx<=monTable.length; idx++){
-        // console.log(table["월"]);
-        value = table[idx];
-        
-    
-       for (var i = 0; i < value.length; i++) {
-          colcell = idx+1;
-        //    console.log('here');
-           var diff= value[i].charCodeAt(0) //ASCII Code로 변환함
-           var long=0;
-           if(Number(diff)>=65 && Number(diff)<=70){ //A~F의 값일 경우 조건문
-               var pluscell= Number(diff)-Number('A'.charCodeAt(0))
-               rowcell=1+3*pluscell
-               long=3
-           }
-           else { //문자가 아닌 숫자형식
-               var time_num = Number(value[i])
-               var pluscell = time_num - 1 //2.5-1 =1.5
-               rowcell = 1 + pluscell * 2
-               long=2
-           }
-
-           for(var j=0;j<long;j++){
-                var cells = rows[rowcell+j].getElementsByTagName("td");
-                // console.log(cells);
-                cells[colcell].innerHTML = "수업시간";
-            }
-
-       } 
-    }
-
     var mergeItem = ""; //병합구분값
     var mergeCount = 0; //병합 수
     var mergeRowNum = 0;  //병합들num갈 r1w
@@ -87,7 +61,7 @@ function find(){
                 var item = $(':eq(' + num +')',$(this)).html();
                 if(mergeItem != item  ) {
                     mergeCount = 1;
-                    mergeItem = item ;
+                    mergeItem = item;
                     mergeRowNum = row;
                 }else{
                     mergeCount = mergeCount + 1;
@@ -97,4 +71,6 @@ function find(){
             }
         })
     }
+
 }
+
