b3e7e738c1f6c129d8af886e50b11efa9f7f8cd6
ShinSeungheon <gody8756@ajou.ac.kr>
2021-06-06 14:01:09 +0900

    update empty class

--- modules/empty_class.js
+++ modules/empty_class.js
@@ -4,50 +4,24 @@ const empty = async(index) => {
       $.getJSON(`../data/sample_empty/student${i}.json`, function(data){//제이슨 파일 읽기
         var rows = document.getElementById("time_table").getElementsByTagName("tr");
         var cells = rows[4].getElementsByTagName("td");
-    
-         for (var j = 0; j < data.length; j++) {//과목수만큼 반복
-            for (var k = 0; k < data["activities"][j]["classdate"].length; k++) {//시간수만큼 반복
-              switch(data["activities"][j]["classdate"][k]){
-                  case '월':
-                      colcell = 1;
-                      break;
-                  case '화':
-                      colcell = 2;
-                      break;
-                  case '수':
-                      colcell = 3;
-                      break;
-                  case '목':
-                      colcell = 4;
-                      break;
-                  case '금':
-                      colcell = 5;
-                      break;
-                  default:
-                      alert("error!");
-              }
-              var diff= data["activities"][j]["start"][k].charCodeAt(0) //ASCII Code로 변환함
-              var long=0;
-              if(Number(diff)>=65 && Number(diff)<=70){ //A~F의 값일 경우 조건문
-                  var pluscell= Number(diff)-Number('A'.charCodeAt(0))
-                  rowcell=1+3*pluscell
-                  long=3
-              }
-              else { //문자가 아닌 숫자형식
-                  var time_num = Number(data["activities"][j]["start"][k])
-                  var pluscell = time_num - 1 //2.5-1 =1.5
-                  rowcell = 1 + pluscell * 2
-                  long=2
-              }
+        $.each(data,function(key1,value1){
+            $.each(value1,function(key,value){
+                console.log(value);
 
-              //value.long[j]=long
-              for(var q=0;q<long;q++){
-                  var cells = rows[rowcell+q].getElementsByTagName("td");
-                  cells[colcell].innerHTML = " ";
-                  cells[colcell].style.backgroundColor = "#ffffff";
-              }
-            }
-         }
+                var colcell = value.classdate;
+                var long = value.long;
+                var rowcell = value.start;
+                
+                //value.long[j]=long
+                for(var i=0;i<long;i++){
+                    var cells = rows[rowcell+i].getElementsByTagName("td");
+                    cells[colcell].innerHTML = " ";
+                    cells[colcell].style.backgroundColor = "#ffffff";
+                }
+
+            });
+        });
+         
       });
    
     }
