88b56e01168a5365833bdcc25a9d5739c9316b1d
ShinSeungheon <gody8756@ajou.ac.kr>
2021-06-06 13:51:34 +0900

    update json data format

---
+++ modules/jsonChanger.js
@@ -0,0 +1,58 @@
+var a = [];
+
+function change(data) {
+    console.log('asdf');
+    jsonData = JSON.parse(data);
+
+    for(var i=0; i<jsonData['activities'].length; i++) {
+        for(var j=0; j<jsonData['activities'][i]['classdate'].length; j++) {
+            act = jsonData['activities'][i];
+            var newAct = {'activities' : ''};
+            newAct['activities'] = {'name':'', 'classdate':'', 'start':'', 'long':'', 'alarm':''};
+            newAct['activities']['name'] = act['name'];
+            newAct['activities']['classdate'] = datToInt(jsonData['activities'][i]['classdate'][j]);
+
+            var rowcell;
+            var diff= jsonData['activities'][i]['start'][j].charCodeAt(0) //ASCII Code로 변환함
+            var long=0;
+            if(Number(diff)>=65 && Number(diff)<=70){ //A~F의 값일 경우 조건문
+                var pluscell= Number(diff)-Number('A'.charCodeAt(0));
+                rowcell=1+3*pluscell;
+                long=3;
+            }
+            else { //문자가 아닌 숫자형식
+                var time_num = Number(jsonData['activities'][i]['start'][j]);
+                var pluscell = time_num - 1; //2.5-1 =1.5
+                rowcell = 1 + pluscell * 2;
+                long=2;
+            }
+            
+            newAct['activities']['start'] = rowcell;
+            newAct['activities']['long'] = long;
+            newAct['activities']['alarm'] = 'Y';
+
+            a.push(newAct);
+        }
+
+    }
+    return JSON.stringify(a, null, 4);
+}
+
+function datToInt(day) {
+    switch(day) {
+        case '월':
+            return 1;
+        case '화':
+            return 2; 
+        case '수':
+            return 3;
+        case '목':
+            return 4;
+        case '금':
+            return 5;
+        default:
+            break;
+    }
+}
+
+module.exports.change = change;
--- printTT.js
+++ printTT.js
@@ -7,11 +7,6 @@ socket.on('recMsg', (data)=> {
     setTimeout(printTable(), 500);
 });
 
-socket.on('logout', (data) => {
-    resetTable();
-})
-
-
 function resetTable() {
     
 }
@@ -30,72 +25,42 @@ function printTable() {
         var rows = document.getElementById("time_table").getElementsByTagName("tr");
         var cells = rows[4].getElementsByTagName("td");
         var a = [];
-        var i = 0;
+        
         $.each(data,function(key1,value1){
             $.each(value1,function(key,value){
-                var rowcell = 1;
-                var colcell = 0;
-                for(var j = 0; j < value.classdate.length ; j++){
-                    switch(value.classdate[j]){
-                        case '월':
-                            colcell = 1;
-                            break;
-                        case '화':
-                            colcell = 2;
-                            break;
-                        case '수':
-                            colcell = 3;
-                            break;
-                        case '목':
-                            colcell = 4;
-                            break;
-                        case '금':
-                            colcell = 5;
-                            break;
-                        default:
-                            alert("error!");
-                    }
-                    var diff= value.start[j].charCodeAt(0) //ASCII Code로 변환함
-                    var long=0;
-                    if(Number(diff)>=65 && Number(diff)<=70){ //A~F의 값일 경우 조건문
-                        var pluscell= Number(diff)-Number('A'.charCodeAt(0))
-                        rowcell=1+3*pluscell
-                        long=3
-                    }
-                    else { //문자가 아닌 숫자형식
-                        var time_num = Number(value.start[j])
-                        var pluscell = time_num - 1 //2.5-1 =1.5
-                        rowcell = 1 + pluscell * 2
-                        long=2
-                    }
+                console.log(value);
 
-                    //value.long[j]=long
-                    for(var i=0;i<long;i++){
-                        var cells = rows[rowcell+i].getElementsByTagName("td");
-                        cells[colcell].innerHTML = value.name;
-                    }
+                var colcell = value.classdate;
+                var long = value.long;
+                var rowcell = value.start;
+                
+                //value.long[j]=long
+                for(var i=0;i<long;i++){
+                    var cells = rows[rowcell+i].getElementsByTagName("td");
+                    cells[colcell].innerHTML = value.name;
                 }
-                i++;
+
             });
         });
-        // var mergeItem = ""; //병합구분값
-        // var mergeCount = 0; //병합 수
-        // var mergeRowNum = 0;  //병합들num갈 r1w
-        // for (var num = 1; num < 7; num++) {
-        //     $('tr','#time_table').each(function(row){  // #테이블ID값
-        //         if(row > 0 ){
-        //             var item = $(':eq(' + num +')',$(this)).html();
-        //             if(mergeItem != item  ) {
-        //                 mergeCount = 1;
-        //                 mergeItem = item ;
-        //                 mergeRowNum = row;
-        //             }else{
-        //                 mergeCount = mergeCount + 1;
-        //                 $("tr:eq("+mergeRowNum+") > td:eq("+num+")").attr("rowspan",mergeCount);
-        //                 $('td:eq('+num+')',$(this)).hide(); //병합될 값들 숨김처리
-        //             }
-        //         }
-        //     })
-        // }
+        
+        for (var num = 1; num < 7; num++) {
+            var mergeItem = "q"; //병합구분값
+            var mergeCount = 0; //병합 수
+            var mergeRowNum = 0;  //병합들num갈 r1w
+            $('tr','#time_table').each(function(row){  // #테이블ID값
+                if(row > 0 ){
+                    var item = $(':eq(' + num +')',$(this)).html();
+                    if(mergeItem != item  ) {
+                        mergeCount = 1;
+                        mergeItem = item ;
+                        mergeRowNum = row;
+                    }else{
+                        mergeCount = mergeCount + 1;
+                        $("tr:eq("+mergeRowNum+") > td:eq("+num+")").attr("rowspan",mergeCount);
+                        $('td:eq('+num+')',$(this)).hide(); //병합될 값들 숨김처리
+                    }
+                }
+            })
+        }
     });
 }
